c63301eb1ac5c468b9cea59c094e73de920ae2af
erocs <github@erocs.org>
2013-02-04 10:05:57 -0800

    WIP

--- src/com/untamedears/DynCap/DynCapPlugin.java
+++ src/com/untamedears/DynCap/DynCapPlugin.java
@@ -1,26 +1,62 @@
 package com.untamedears.DynCap;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
+import java.util.HashSet;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.Set;
+//import javax.crypto.Cipher;
+//import javax.crypto.CipherInputStream;
+//import javax.crypto.spec.IvParameterSpec;
+//import sun.misc.BASE64Decoder;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import org.bukkit.scheduler.BukkitTask;
 
 public class DynCapPlugin extends JavaPlugin implements Listener {
+    public static final int kTicksPerSec = 20;
+    public static final int kMaxFailures = 2;
+
+    public static DynCapPlugin plugin_ = null;
+
+    public static DynCapPlugin getPlugin() {
+        return plugin_;
+    }
+
 	private DynCapCommands commands;
 	private int dynamicPlayerCap = 1000;
 	private Logger log;
-	private boolean whiteListEnabled = false;
+	private boolean bukkitWhiteListEnabled = false;
+    private boolean dyncapWhiteListEnabled = false;
+    private Set<String> dyncapPlayers = new HashSet<String>();
+    private String dyncapPlayersFile;
+    private Integer dyncapPlayersReloadSec;
+    private String dyncapPlayersCryptoAlgorithm;
+    private String dyncapPlayersCryptoPassword;
+    private byte[] dyncapPlayersCryptoIv;
+    private BukkitTask wlReloadTask;
+    private int dyncapPlayersReloadFailures = 0;
 
 	public void onEnable() {
+        plugin_ = this;
 		log = this.getLogger();
 		commands = new DynCapCommands(this, log);
 		
@@ -29,12 +65,49 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 			getCommand(command).setExecutor(commands);
 		}
 
+        this.reloadConfig();
+        FileConfiguration config = this.getConfig();
+        config.options().copyDefaults(true);
+        dynamicPlayerCap = config.getInt("initial-cap", 1000);
+        dyncapPlayersFile = config.getString("dyncap-whitelist-file");
+        dyncapPlayersReloadSec = config.getInt("dyncap-whitelist-interval");
+        dyncapPlayersCryptoAlgorithm = config.getString(
+            "dyncap-whitelist-crypto", "AES/CBC/PKCS5Padding");
+        dyncapPlayersCryptoPassword = config.getString("dyncap-whitelist-password");
+        String encoded_iv = config.getString("dyncap-whitelist-iv");
+
+//        BASE64Decoder decoder = new BASE64Decoder();
+//        dyncapPlayersCryptoIv = decoder.decodeBuffer(encoded_iv);
+
+        scheduleDyncapWhitelistReload();
+
 		// Give the console permission
 		ConsoleCommandSender console = getServer().getConsoleSender();
 		console.addAttachment(this, "dyncap.console", true);
 	}
 
 	public void onDisable() {}
+
+	@EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=false)
+	public void onAsyncPlayerPreLoginEvent(AsyncPlayerPreLoginEvent event) {
+		if (bukkitWhiteListEnabled &&
+			event.getLoginResult() == AsyncPlayerPreLoginEvent.Result.ALLOWED)
+        {
+			return;
+        } else if (dyncapWhiteListEnabled &&
+            !dyncapPlayers.contains(event.getName().toLowerCase()))
+        {
+			event.disallow(
+				AsyncPlayerPreLoginEvent.Result.KICK_OTHER,
+				"Not registered.");
+		} else if (isServerFull()) {
+			event.disallow(
+				AsyncPlayerPreLoginEvent.Result.KICK_FULL,
+				"Server full.");
+		} else {
+			return;
+		}
+	}
 
 	@EventHandler(priority=EventPriority.MONITOR)
 	public void onPlayerJoinEvent(PlayerJoinEvent event) {
@@ -69,7 +142,9 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 	}
 
 	private void setWhitelist(boolean enabled, int playerCount) {
-		if ((enabled && !whiteListEnabled) || (!enabled && whiteListEnabled)) {
+		if ((enabled && !bukkitWhiteListEnabled) ||
+            (!enabled && bukkitWhiteListEnabled))
+        {
 			Integer cap = getPlayerCap();
 			String state_message = "disabled";
 			if (enabled) {
@@ -80,11 +155,125 @@ public class DynCapPlugin extends JavaPlugin implements Listener {
 					playerCount, cap, state_message);
 			log.info(message);
 		}
-		whiteListEnabled = enabled;
+		bukkitWhiteListEnabled = enabled;
 		Bukkit.setWhitelist(enabled);
 	}
 
 	public int getPlayerCount() {
 		return this.getServer().getOnlinePlayers().length;
 	}
+
+    public boolean isServerFull() {
+		return getPlayerCount() >= getPlayerCap();
+    }
+
+    private void scheduleDyncapWhitelistReload() {
+        if (dyncapPlayersFile == null) { return; }
+        wlReloadTask = this.getServer().getScheduler().runTaskTimerAsynchronously(
+            this,
+            new Runnable() {
+                @Override  
+                public void run() {
+                    getPlugin().ReloadDyncapWhitelist();
+                }
+            },
+            1L,
+            dyncapPlayersReloadSec * kTicksPerSec);
+    }
+
+    public void ReloadDyncapWhitelist() {
+        if (dyncapPlayersFile == null) { return; }
+        if (!ReloadDWLFile() && !ReloadDWLUri()) {
+            ReloadDyncapWhitelistFailure();
+        } else {
+            dyncapPlayersReloadFailures = 0;
+        }
+    }
+
+    private boolean ReloadDWLFile() {
+        BufferedReader br = null;
+        try {
+            File f = new File(dyncapPlayersFile);
+            if (!f.exists()) {
+                return false;
+            }
+            br = getDWLReader(new FileReader(f));
+            dyncapPlayers = LoadDWLFromReader(br);
+            return true;
+        } catch (Exception ex) {
+        } finally {
+            try {
+                if (br != null) { br.close(); }
+            } catch (Exception ex) {
+            }
+        }
+        return false;
+    }
+
+    private boolean ReloadDWLUri() {
+        URL url = null;
+        try {
+            url = new URL(dyncapPlayersFile);
+        } catch (Exception ex) {
+            return false;
+        }
+        String protocol = url.getProtocol();
+        if (protocol != "http" && protocol != "https" && protocol != "file") {
+            return false;
+        }
+        BufferedReader br = null;
+        try {
+            InputStreamReader isr = new InputStreamReader(url.openStream(), "UTF-8");
+            br = getDWLReader(isr);
+            dyncapPlayers = LoadDWLFromReader(br);
+            return true;
+        } catch (Exception ex) {
+        } finally {
+            try {
+                if (br != null) { br.close(); }
+            } catch (java.io.IOException ex) {
+            }
+        }
+        return false;
+    }
+
+    private BufferedReader getDWLReader(Reader in) {
+        if (dyncapPlayersCryptoAlgorithm == null ||
+            dyncapPlayersCryptoPassword == null)
+        {
+            return new BufferedReader(in);
+        }
+        log.severe("Not implemented");
+        return null;
+//        Cipher cipher = Cipher.getInstance(dyncapPlayersCryptoAlgorithm);
+//        
+//        cipher.init(Cipher.DECRYPT_MODE, secret, new IvParameterSpec(dyncapPlayersCryptoIv));
+//
+//            CipherInputStream
+
+    }
+
+    private Set<String> LoadDWLFromReader(BufferedReader br) throws IOException {
+        Set<String> new_players = new HashSet<String>();
+        while (br.ready()) {
+            String line = br.readLine().trim().toLowerCase();
+            if (line.length() <= 0) { continue; }
+            if (line.charAt(0) == '#') { continue; }
+            new_players.add(line);
+        }
+        return new_players;
+    }
+
+    private boolean ReloadDyncapWhitelistFailure() {
+        ++dyncapPlayersReloadFailures;
+        if (dyncapPlayersReloadFailures >= kMaxFailures) {
+            // TODO: Ponder failure scenarios. Should it lock down the server
+            //  until a server restart?
+            log.severe("DynCap WhiteList file not found");
+            wlReloadTask.cancel();
+            dyncapPlayersFile = null;
+            return true;
+        }
+        return false;
+    }
 }
